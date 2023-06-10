dc06f744c97b8fc60f7a0e02f40b8f7656addc18
mcpr2593 <mcprr25@gmail.com>
2023-06-10 16:41:53 +0700

    File untuk  membaca data pir sernsor

--- ceksensor.php
+++ ceksensor.php
@@ -1,16 +1,16 @@
 <?php
-    include 'layouts/config.php';
+    include 'layouts/config.php';// meminta data koneksi pada database
 
 
-    $sql = mysqli_query($link, "SELECT * FROM controler");
+    $sql = mysqli_query($link, "SELECT * FROM controler"); // meminta data dari tabel controler
     $data = mysqli_fetch_assoc($sql);
-    $value = $data["pir_sensor"];
+    $value = $data["pir_sensor"]; //mengabil data colum pir_sensor dari tabel controler 
 
 
     if ( $value == 1 ){
-        $value = "<b>Ada Orang di Ruangan</b>";
+        $value = "<b>Ada Orang di Ruangan</b>"; //menampilkan text jika value sensor 1
     } else {
-        $value = "<b>Ruangan Kosong</b>";
+        $value = "<b>Ruangan Kosong</b>"; //menampilkan text jika value sensor 0
     }
     echo $value;
 ?>
